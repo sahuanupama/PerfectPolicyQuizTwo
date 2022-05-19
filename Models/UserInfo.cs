e39b59e74d1d917fbef41f439bcbfd2b167f01b2
sahuanupama <anupamasahu@outlook.com>
2022-05-20 00:28:02 +1000

    Fixed UI Error

--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -9,6 +9,5 @@ namespace PerfectPolicyQuizTwo.Models
     {
         public string Username { get; set; }
         public string Password { get; set; }
-        public int UserInfoId { get; set; }
     }
 }
