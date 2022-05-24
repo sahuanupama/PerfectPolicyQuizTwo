a7a98e3c9c5a9a44218fd8323a023bec4309cd51
Anupama Sahu <anupamasahu@outlook.com>
2022-05-25 08:35:35 +1000

    Fixed file upload

--- Controllers/QuestionController.cs
+++ Controllers/QuestionController.cs
@@ -1,13 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using PerfectPolicyQuizTwo.Helper;
 using PerfectPolicyQuizTwo.Models;
 using PerfectPolicyQuizTwo.Services;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace PerfectPolicyQuizTwo.Controllers
@@ -17,11 +16,13 @@ namespace PerfectPolicyQuizTwo.Controllers
         private readonly IApiRequest<Question> _apiQuestionRequest;
         private readonly IApiRequest<Quiz> _apiQuizRequest;
         private readonly string questionController = "Question";
+        private IWebHostEnvironment _environment;
 
-        public QuestionController(IApiRequest<Question> apiQuestionRequest, IApiRequest<Quiz> apiQuizRequest)
+        public QuestionController(IApiRequest<Question> apiQuestionRequest, IApiRequest<Quiz> apiQuizRequest, IWebHostEnvironment environment)
         {
             _apiQuestionRequest = apiQuestionRequest;
             _apiQuizRequest = apiQuizRequest;
+            _environment = environment;
         }
 
         // GET: QuestionController
@@ -152,6 +153,29 @@ namespace PerfectPolicyQuizTwo.Controllers
         {
             List<Question> quizQuestions = _apiQuestionRequest.GetAllForParentId("Question", "QuestionsForQuizId", id);
             return View("Index", quizQuestions);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            try
+            {
+                // retrive a folder path
+                string folderRoot = Path.Combine(_environment.ContentRootPath, "wwwroot\\Uploads");
+                //combine filename and folder path
+                string filePath = Path.Combine(folderRoot, file.FileName);
+                //save the file
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+                return Ok(new { success = true, message = "File Uploaded" });
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(new { success = false, message = e.Message });
+            }
         }
     }
 }
--- Controllers/QuizController.cs
+++ Controllers/QuizController.cs
@@ -5,13 +5,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using PerfectPolicyQuizTwo.Helper;
 using PerfectPolicyQuizTwo.Models;
 using PerfectPolicyQuizTwo.Services;
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
-
 
 namespace PerfectPolicyQuizTwo.Controllers
 {
@@ -21,14 +15,11 @@ namespace PerfectPolicyQuizTwo.Controllers
         private readonly IApiRequest<Question> _apiQuestionRequest;
         private readonly string quizController = "Quiz";
 
-        private IWebHostEnvironment _environment;
-
         public QuizController(IApiRequest<Quiz> apiQuizRequest, IApiRequest<Question> apiQuestionRequest, IWebHostEnvironment environment)
 
         {
             _apiQuizRequest = apiQuizRequest;
             _apiQuestionRequest = apiQuestionRequest;
-            _environment = environment;
         }
 
         [HttpPost]
@@ -176,37 +167,9 @@ namespace PerfectPolicyQuizTwo.Controllers
             // retrieve a list of all teachers
             var quizList = _apiQuizRequest.GetAll(quizController);
             // filter that list, return the results to a new list
-            var filteredList = quizList.Where(c => c.QuizTitle.ToLower().Contains(filterText.ToLower())).ToList();
+            var filteredList = quizList.Where(c => c.QuizPersonName.ToLower().Contains(filterText.ToLower())).ToList();
             // return this list to the index page
             return View("Index", filteredList);
         }
-
-        [HttpPost]
-        public async Task<IActionResult> UploadFile(IFormFile file)
-        {
-            try
-            {
-                // retrive a folder path
-                string folderRoot = Path.Combine(_environment.ContentRootPath, "wwwroot\\Uploads");
-                //combine filename and folder path
-                string filePath = Path.Combine(folderRoot, file.FileName);
-                //save the file
-                using (var stream = new FileStream(filePath, FileMode.Create))
-                {
-                    await file.CopyToAsync(stream);
-                }
-                return Ok(new { success = true, message = "File Uploaded" });
-            }
-            catch (Exception e)
-            {
-
-                return BadRequest(new { success = false, message = e.Message });
-            }
-
-
-        }
-
-
-
     }
 }
